137204f33f36de83840fca40ed57a776140b7321
Kristian Kinderlöv <kristian.kinderlov@airolit.com>
2025-02-05 13:59:16 +0100

    Change to swedish keyboard

--- nixos/_mixins/desktop/gnome.nix
+++ nixos/_mixins/desktop/gnome.nix
@@ -4,4 +4,5 @@
 
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.xkb.layout = "se";  # Sets Swedish keyboard layout
 }
